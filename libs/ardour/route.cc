00067c7f832f9d1d2a6b7e4196e0ed3e9eae0adb
Paul Davis <paul@linuxaudiosystems.com>
2018-08-13 08:57:22 -0400

    actually add and position a beatbox processor to midi tracks

--- libs/ardour/route.cc
+++ libs/ardour/route.cc
@@ -41,6 +41,7 @@
 #include "ardour/audio_track.h"
 #include "ardour/audio_port.h"
 #include "ardour/audioengine.h"
+#include "ardour/beatbox.h"
 #include "ardour/boost_debug.h"
 #include "ardour/buffer.h"
 #include "ardour/buffer_set.h"
@@ -2906,6 +2907,9 @@ Route::set_processor_state (const XMLNode& node)
 		} else if (prop->value() == "trim") {
 			_trim->set_state (**niter, Stateful::current_state_version);
 			new_order.push_back (_trim);
+		} else if (prop->value() == "beatbox" && _beatbox) {
+			_beatbox->set_state (**niter, Stateful::current_state_version);
+			new_order.push_back (_beatbox);
 		} else if (prop->value() == "meter") {
 			_meter->set_state (**niter, Stateful::current_state_version);
 			new_order.push_back (_meter);
@@ -4730,6 +4734,12 @@ Route::setup_invisible_processors ()
 
 	for (ProcessorList::iterator i = _processors.begin(); i != _processors.end(); ++i) {
 		boost::shared_ptr<Send> auxsnd = boost::dynamic_pointer_cast<Send> ((*i));
+
+		/* XXX temporary hack while we decide on visibility */
+		if (boost::dynamic_pointer_cast<BeatBox> (*i)) {
+			continue;
+		}
+
 		if ((*i)->display_to_user ()) {
 			new_processors.push_back (*i);
 		}
@@ -4838,6 +4848,14 @@ Route::setup_invisible_processors ()
 		assert (!_trim->display_to_user ());
 		new_processors.push_front (_trim);
 		trim = new_processors.begin();
+	}
+
+	/* BEATBOX (for MIDI) */
+
+	if (_beatbox) {
+		ProcessorList::iterator insert_pos = trim;
+		++insert_pos;
+		new_processors.insert (insert_pos, _beatbox);
 	}
 
 	/* INTERNAL RETURN */
